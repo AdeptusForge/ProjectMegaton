bf2086e12d8092d157339fe7e263b858396d9255
Dunamis <adeptusforgeceo@gmail.com>
2022-04-23 14:45:33 -0700

    Rebase Commit

--- MADRengine/MADRengine/src/main.cpp
+++ MADRengine/MADRengine/src/main.cpp
@@ -2,17 +2,3 @@
 #include "main.h"
 
 
-int main() 
-{
-	while(true) 
-	{
-		TestTest();
-
-	}
-	return 0;
-}
-
-void TestTest()
-{
-	std::cout << "This is TestTest from MADR" << std::endl;
-}
--- MADRengine/ProjectMegaton/src/ProjectMegaton/main.cpp
+++ MADRengine/ProjectMegaton/src/ProjectMegaton/main.cpp
@@ -1,2 +1,16 @@
 #include "pmpch.h"
 
+void TestTest()
+{
+	std::cout << "This is TestTest from MADR" << std::endl;
+}
+
+int main()
+{
+	while (true)
+	{
+		TestTest();
+
+	}
+	return 0;
+}
